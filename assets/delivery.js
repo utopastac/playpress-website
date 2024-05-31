6b4c62d8acba5457b3f3c0d2396144101d351063
Peter Wright <utopastac@gmail.com>
2024-05-31 18:47:08 +0100

    Changing delivery info box

--- assets/delivery.js
+++ assets/delivery.js
@@ -1,11 +1,13 @@
 
 window.addEventListener("load", function(){
   const delivery = document.querySelector('#delivery');
+  const deliveryIcon = document.querySelector('#delivery-icon');
   var deliveryActive = false;
   delivery.addEventListener('click', toggleDelivery);
 
   function toggleDelivery(){
     deliveryActive = !deliveryActive;
     deliveryActive ? delivery.classList.add("active") : delivery.classList.remove("active");
+    deliveryActive ? deliveryIcon.classList.add("minus") : deliveryIcon.classList.remove("minus");
   }
 });
