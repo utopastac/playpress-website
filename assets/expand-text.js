758ded61147e3e7997c98be926c22d15fadadb09
Peter Wright <utopastac@gmail.com>
2025-05-31 13:51:18 -0400

    Changingproduct page to have less distraction

--- assets/expand-text.js
+++ assets/expand-text.js
@@ -1,34 +1,12 @@
 window.addEventListener('load', () => {
-  const textElement = document.getElementById("description-text");
-  const originalTextNodes = textElement.querySelectorAll("p");
-  const readMore = document.createElement("p");
-  readMore.innerHTML = "Full description";
-  readMore.classList.add("read-more");
-  readMore.addEventListener('click', (e) => {
-    switchText();
-  });
-  textElement.append(readMore);
-  let hidden = false;
-  switchText();
-  
+  const description = document.querySelector('#description');
+  const descriptionIcon = document.querySelector('#description-icon');
+  var descriptionActive = false;
+  description.addEventListener('click', toggleDescription);
 
-  
-
-  function switchText(){
-    // textElement.innerHTML = "";
-    hidden = !hidden;
-    
-    if(hidden){
-      // for (var i = 0; i < originalTextNodes.length; i++) {
-      //   textElement.append(originalTextNodes[i]);
-      // }
-      textElement.classList.add("hidden")
-      readMore.innerHTML = "Full description";
-    } else {
-      // textElement.append(originalTextNodes[0]);
-      // textElement.append(readMore);
-      textElement.classList.remove("hidden")
-      readMore.innerHTML = "Close";
-    }
+  function toggleDescription(){
+    descriptionActive = !descriptionActive;
+    descriptionActive ? description.classList.add("active") : description.classList.remove("active");
+    descriptionActive ? descriptionIcon.classList.add("minus") : descriptionIcon.classList.remove("minus");
   }
 });
