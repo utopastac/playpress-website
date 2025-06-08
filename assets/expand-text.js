24bc15dbc2458e399289ba681c8921896e1f2d36
Peter Wright <utopastac@gmail.com>
2025-06-08 10:46:25 -0400

    Reviews on page

--- assets/expand-text.js
+++ assets/expand-text.js
@@ -9,4 +9,15 @@ window.addEventListener('load', () => {
     descriptionActive ? description.classList.add("active") : description.classList.remove("active");
     descriptionActive ? descriptionIcon.classList.add("chevron-up") : descriptionIcon.classList.remove("chevron-up");
   }
+
+  const reviews = document.querySelector('#reviews');
+  const reviewsIcon = document.querySelector('#reviews-icon');
+  var reviewsActive = false;
+  reviews.addEventListener('click', toggleReviews);
+
+  function toggleReviews(){
+    reviewsActive = !reviewsActive;
+    reviewsActive ? reviews.classList.add("active") : reviews.classList.remove("active");
+    reviewsActive ? reviewsIcon.classList.add("chevron-up") : reviewsIcon.classList.remove("chevron-up");
+  }
 });
